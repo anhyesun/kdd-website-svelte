bb804268350f3bc705d1c06fc923dd94c347bc86
Eric Kim <bosung90@gmail.com>
2023-01-11 22:40:10 -0800

    add kdd images

---
+++ src/lib/images/socialIcons.ts
@@ -0,0 +1,4 @@
+export {default as dribbble} from './socialIcons/dribbble.svg'
+export {default as github} from './socialIcons/github.svg'
+export {default as linkedin} from './socialIcons/linkedin.svg'
+export {default as other} from './socialIcons/link.svg'
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script>
+  import kddLogo1 from '$lib/images/logo_kr_white_horizontal.svg'
+  import kddLogo2 from '$lib/images/logo_kr_color_horizontal.png'
+  import {page} from '$app/stores'
+  $: isHome = $page.url.pathname === '/'
+  import './styles.scss'
+
+  const color = 'blue'
+</script>
+
+<div style="--heading: {color}" />
+<nav class:isHome>
+  <a href="/"><img src={kddLogo1} alt="kdd logo" /></a>
+  <a href="/about-us">About Us</a>
+  <a href="/events">Events</a>
+  <a href="/photos">Photos</a>
+  <h1>hell</h1>
+</nav>
+
+<slot />
+
+<style lang="scss">
+  :root {
+    --heading: yellow;
+  }
+  h1 {
+    color: var(--heading);
+  }
+  nav {
+    align-items: center;
+    padding: 1px;
+    padding-top: 5px;
+    z-index: 1200;
+    width: 100%;
+    position: relative;
+    a {
+      color: var(--blue);
+    }
+    img {
+      width: 140px;
+    }
+    background-color: pink;
+  }
+  .isHome {
+    // position: absolute;
+    > a {
+      color: white;
+    }
+  }
+</style>
