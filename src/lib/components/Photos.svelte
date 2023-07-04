09a7b88cc1a7f64622359c68951b67a55bd701af
Eric Kim <bosung90@gmail.com>
2023-07-05 08:28:28 +0900

    fix photos page photo and photo dialog style

--- src/lib/components/Photos.svelte
+++ src/lib/components/Photos.svelte
@@ -10,8 +10,10 @@
 {#each dbPhotos as { title, description, photos, id } (id)}
   {#if photos}
     {#each photos as photo (photo.id)}
-      <button on:click={() => (selectedPhotoAttachment = photo)}>
-        <div class="bg-slate-200 h-72 w-96 overflow-clip rounded">
+      <button
+        on:click={() => (selectedPhotoAttachment = photo)}
+        class="flex-col items-center w-96 gap-2">
+        <div class="bg-slate-200 h-72 w-full overflow-clip rounded">
           <img
             class="w-full h-full object-cover hover:scale-125 transition-transform"
             src={photo.url}
@@ -31,9 +33,9 @@
   on:click={() => dialog.close()}
   on:close={() => (selectedPhotoAttachment = null)}
   class="bg-transparent backdrop-blur-sm max-w-full max-h-full">
-  <div class="w-screen h-screen p-20">
+  <div class="w-screen h-screen p-10 md:p-20 flex-center">
     <img
-      class="w-full h-full object-contain rounded"
+      class="bg-slate-200 max-w-full max-h-full object-contain rounded"
       src={selectedPhotoAttachment?.url}
       alt={selectedPhotoAttachment?.filename} />
   </div>
